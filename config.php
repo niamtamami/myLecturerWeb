60a3f031fffadd6bf097ccc868349bd097648a1d
nitbot <niamtamami@gmail.com>
2015-11-05 18:36:12 -0800

    V.0.0
    seluruh sistem sempurna, tetapi belum termasuk revisi slider gambar

---
+++ config.php
@@ -0,0 +1,63 @@
+<?php
+$myweb = "http://nitbot.com/";
+$myfacebook="http://facebook.com/niamtamami";
+$mygithub="https://github.com/nitbot/";
+$myyoutube="https://www.youtube.com/channel/UCcvaQtaNhx5G_OKXT6I3RAg";
+$myblog="http://blog.nitbot.com/";
+$myabout="http://niam.lecturer.pens.ac.id/";
+$myemail="niam@pens.ac.id";
+$mygmail="niamtamami@gmail.com";
+$myrepo="https://goo.gl/T5v1BK";
+$myteachingmaterial = "https://goo.gl/uIA30J";
+
+$myPersonalDescrip="
+<strong>Ni'am Tamami</strong> was born in Kediri, East Java, Indonesia in 1990. He received an S.ST. (Bachelor) degree in Electronics Engineering from Politeknik Elektronika Negeri Surabaya (PENS) in 2012. He joined Politeknik Elektronika Negeri Surabaya (PENS) as lecturer in 2012. He received an M.T. (Master) degree in Electronics Engineering from Politeknik Elektronika Negeri Surabaya (PENS) in 2014. He is now still active as D3 and D4 (3 and 4 years diploma) program lecturer, especially in Electronic Department. His teaching subjects are including ...
+";
+
+$myResearchTitle1="Robo nitbot";
+$myResearchDescrip1="a balancing robot with image processing";
+
+$myResearchTitle2="Internet Of Things | intel Galileo";
+$myResearchDescrip2="send all data in internet";
+
+$myResearchTitle3="nitbot UAV";
+$myResearchDescrip3="unique UAV";
+
+$myJournal = array();
+$myJournal[1] = "Tamami, Niam, Endra Pitowarno, and I. Gede Puja Astawa. “A Combination of PD Controller and PIAFC for Stabilization of “x” Configuration Quadcopter.”, EMITTER International Journal of Engineering Technology, Vol 02, No. 01, 2014. ";
+$myJournal[0] = "Tamami, Niam, Endra Pitowarno, and I. Gede Puja Astawa. “Proportional Derivative Active Force Control for “X” Configuration Quadcopter.”, Journal of Mechatronics, Electrical Power, and Vehicular Technology 5, no. 2 (2014): 67-74.";
+
+$myPublication = array();
+$myPublication[1] ="Tamami, Niam, Endra Pitowarno, and I. Gede Puja Astawa. “Modelling and PD Control for “x” Configuration Quadcopter”, Indonesian Symposium on Robot Soccer Competition 2013 – June 6th , 2013."; 
+$myPublication[0] ="Bambang Sumantri, Ni’am Tamami, Nofria Hanafi, Mohamad Nasyir Tamara and Endra Pitowarno. “Rancang Bangun Aktuator Pada Prototype Picosatellite Menggunakan Sistem Magnetorquer.”, The 14th Industrial Electronics Seminar 2012 (IES 2012) Electronic Engineering Polytechnic Institute of Surabaya (EEPIS), Indonesia, October 24, 2012 : 22-27";
+
+$miniArtikel1=array();
+$miniArtikel1[0]="my Internet of Things";
+$miniArtikel1[1]="Still underConstruction";
+
+$miniArtikel2=array();
+$miniArtikel2[0]="my Student";
+$miniArtikel2[1]="Still underConstruction";
+
+$miniArtikel3=array();
+$miniArtikel3[0]="my Robot";
+$miniArtikel3[1]="Still underConstruction";
+
+$miniArtikel4=array();
+$miniArtikel4[0]="my Family";
+$miniArtikel4[1]="Still underConstruction";
+
+$miniArtikel5=array();
+$miniArtikel5[0]="my Lab";
+$miniArtikel5[1]="Still underConstruction";
+
+$miniArtikel6=array();
+$miniArtikel6[0]="my Theaching Experience";
+$miniArtikel6[1]="Still underConstruction";
+?>
+
+
+
+
+
+
